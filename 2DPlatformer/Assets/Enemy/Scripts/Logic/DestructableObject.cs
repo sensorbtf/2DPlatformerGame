ff542bccc8dd203eb4c715bf828051b61a4ca569
aniahuber <aniahuber13@gmail.com>
2023-04-07 13:58:44 +0200

    Added Barrel - Destructable Object
    Added Barrel with script and animations.

---
+++ 2DPlatformer/Assets/Enemy/Scripts/Logic/DestructableObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableObject : Enemy
+{
+    protected override void OnTriggerEnter2D(Collider2D collision) { }
+}
