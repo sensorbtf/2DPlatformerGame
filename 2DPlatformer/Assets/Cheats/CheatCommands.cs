aa36fab55825ef3a35175c849be263cb321c9f33
aniahuber <aniahuber13@gmail.com>
2023-04-24 16:55:27 +0200

    Added cheats
    for my own sanity.
    F1 - collect all notes + kill all enemies
    F2 - player is untouchable
    F3 - disable last level spikes

---
+++ 2DPlatformer/Assets/Cheats/CheatCommands.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCommands : MonoBehaviour
+{
+
+    private GameObject playerObject;
+    private GameObject DeadlySpikesWall;
+
+    private void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        DeadlySpikesWall = GameObject.FindGameObjectWithTag("DeadlySpikesWall");
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                Destroy(enemy);
+            }
+
+            GameObject[] musicNotes = GameObject.FindGameObjectsWithTag("Note");
+            foreach (GameObject musicNote in musicNotes)
+            {
+                Destroy(musicNote);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            if (playerObject.CompareTag("Player"))
+            {
+                playerObject.tag = "Untagged";
+            }
+            else
+            {
+                playerObject.tag = "Player";
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            if (DeadlySpikesWall.active == true)
+            {
+                DeadlySpikesWall.active = false;
+            }
+            else
+            {
+                DeadlySpikesWall.active = true;
+            }
+        }
+    }
+}
